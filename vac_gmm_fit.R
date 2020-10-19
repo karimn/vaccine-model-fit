74eecb6e052e21ab0591a192159155b5e1ffeb7b
Karim Naguib <karimn2.0@gmail.com>
2020-10-19 13:29:21 -0400

    Testing with optim() only

--- vac_gmm_fit.R
+++ vac_gmm_fit.R
@@ -3,10 +3,11 @@ library(magrittr)
 library(tidyverse)
 library(gmm)
 library(furrr)
+library(R6)
 
 library(vaccineEarlyInvest)
 
-plan(multisession)
+# plan(multisession)
 
 # Load the CGD data and convert it ----------------------------------------
 
@@ -31,10 +32,6 @@ load_candidate_data <- function(data_file) {
   candidate_data$Target[1:5]<-"Spike"
   candidate_data$Target[10:15]<-"Recombinant"
   
-  # candidate_data %>% 
-  #   group_nest(Platform, Subcategory, Target, .key = "candidates") %>% 
-  #   mutate(vacc_group_id = seq(n()))
-  
   return(candidate_data)
 }
 
@@ -46,16 +43,6 @@ get_candidate_draws <- function(candidate_data, replications,
     par <- Parameters$new(replications = replications, 
                           poverall = poverall, psubcat = psubcat, pvector = pvector, psubunit = psubunit, prna = prna, pdna = pdna, pattenuated = pattenuated, 
                           pinactivated = pinactivated, ppreclinical = ppreclinical, pphase1 = pphase1, pphase2 = pphase2, pphase3 = pphase3, maxcand = maxcand)  
-    dordered <- candidate_data %>% 
-      candidatesFung(par) %>% 
-      pluck("dordered")
-    
-    dplatforms <- unique(dordered[, .(Platform, pplat)])
-    setkey(dplatforms, Platform, pplat)
-  
-    dordered <- dordered[,1:11]
-    dcandidate <- copy(dordered)
-    
     vacc_group_ids <- candidate_data %>%
       pivot_longer(Phase1candidates:PreClinicalCandidates, names_to = "phase") %>% # Add phase to ID columns
       filter(value > 0) %>% 
@@ -63,7 +50,11 @@ get_candidate_draws <- function(candidate_data, replications,
       group_by_at(group_vaccines_by) %>% 
       summarize(vacc_group_id = cur_group_id(), .groups = "drop")
    
-    draws <- candidateDraws(dcandidate, par, seed = seed) %>% 
+    draws <- candidate_data %>% 
+      candidatesFung(par) %>% 
+      pluck("dordered") %>% 
+      select(1:11) %>% 
+      candidateDraws(par, seed = seed) %>% 
       left_join(vacc_group_ids, by = setdiff(names(vacc_group_ids), "vacc_group_id")) %>% # Get the vaccine group IDs
       group_by(r, vacc_group_id, .drop = FALSE) %>% 
       summarize(any_success = sum(success) > 0, 
@@ -77,23 +68,27 @@ summarize_draws <- function(draws) {
     success_rates <- draws %>% 
       group_by(vacc_group_id) %>% 
       summarize(
-        # success_rate = mean(any_success),
         success_rate = mean(success_rate),
-        success_rate_pred = qlogis(success_rate),
         
         .groups = "drop"
       ) %>% 
       ungroup()
     
     success_corr <- draws %>% 
-      # pivot_wider(id_cols = r, names_from = vacc_group_id, values_from = any_success) %>% 
       pivot_wider(id_cols = r, names_from = vacc_group_id, values_from = success_rate) %>% 
       select(-r) %>% 
       as.data.frame() %>% 
       pcaPP::cor.fk() %>% # Faster than base cor()
       magrittr::extract(lower.tri(.)) # Get lower triangle
     
-    lst(success_rates, success_corr)
+    success_vcov <- draws %>% 
+      pivot_wider(id_cols = r, names_from = vacc_group_id, values_from = success_rate) %>% 
+      select(-r) %>% 
+      as.data.frame() %>% 
+      cov() %>% 
+      magrittr::extract(lower.tri(.)) # Get lower triangle
+    
+    lst(success_rates, success_corr, success_vcov)
 }
 
 calculate_draws_vcov <- function(draws) {
@@ -103,41 +98,60 @@ calculate_draws_vcov <- function(draws) {
     cov() 
 }
 
-build_gmm_g <- function(candidate_data, replications, maxcand, use_corr_moments = FALSE, group_vaccines_by = vars(Platform, Subcategory), sim_seed = NULL, moments_to_use = everything()) {
-  function(pbeta, cgd_trials) {
+OptimLogger <- R6Class("OptimLogger", 
+                       list(
+                         data = tibble(),
+                         log = function(...) { self$data %<>% bind_rows(tibble(!!!rlang::list2(...))) }))
+
+build_gmm_g <- function(candidate_data, x, replications, maxcand, use_vcov_moments = FALSE, group_vaccines_by = vars(Platform, Subcategory), sim_seed = NULL, moments_to_use = everything(), fixed_model_probs = NULL, logger = NULL, calculate_objective = FALSE) {
+  function(pbeta, ...) {
     model_probs <- plogis(pbeta) %>% 
-      set_names(c("poverall", "psubcat", "pvector", "psubunit", "prna", "pdna", "pattenuated", "pinactivated", 
-                  "ppreclinical", "pphase1", "pphase2", "pphase3"))
+      # list(poverall = .) %>% 
+      as.list() %>% 
+      list_modify(!!!fixed_model_probs)
     
-    print(unname(model_probs))
+    model_probs %>% 
+      imap(~ str_c(.y, " = ", .x)) %>% 
+      str_c(collapse = ", ") %>% 
+      cat("\n")
     
     draws <- rlang::exec(get_candidate_draws, candidate_data, replications = replications, !!!model_probs, maxcand = maxcand, group_vaccines_by = group_vaccines_by, seed = sim_seed)  
  
     model_summaries <- summarize_draws(draws) 
     
-    # moments <- full_join(model_summaries$success_rates, cgd_trials$success_rates, by = "vacc_group_id") %>%
-    #   mutate_at(vars(starts_with("success_rate")), coalesce, 0) %>%
-    #   transmute(vacc_group_id, 
-    #             success_moment = success_rate.x - success_rate.y) %>% 
-    #   pull(success_moment)
-    # 
-    # if (use_corr_moments) { 
-    #   known_corr <- !is.na(cgd_trials$success_corr)
-    #   
-    #   moments %<>% 
-    #     c(model_summaries$success_corr[known_corr] - cgd_trials$success_corr[known_corr]) %>%
-    #     coalesce(0.0)
-    # }
-    # 
-    # return(t(moments))
-    
-    full_join(cgd_trials, model_summaries$success_rates, by = "vacc_group_id") %>%
+    moments <- full_join(model_summaries$success_rates, x$success_rates, by = "vacc_group_id") %>%
       mutate_at(vars(starts_with("success_rate")), coalesce, 0) %>%
-      transmute(r, vacc_group_id, m = success_rate.x - success_rate.y) %>%
-      pivot_wider(names_from = vacc_group_id, values_from = m) %>%
-      select(-r) %>%
-      mutate_all(coalesce, 0) %>% 
-      select(moments_to_use)
+      transmute(vacc_group_id,
+                success_moment = success_rate.x - success_rate.y) %>%
+      pull(success_moment)
+    
+    if (use_vcov_moments) {
+      moments %<>% 
+        c(model_summaries$success_vcov - x$success_vcov) 
+    }
+    
+    moments %<>% t() 
+
+   # moments <- full_join(x, model_summaries$success_rates, by = "vacc_group_id") %>%
+   #    mutate_at(vars(starts_with("success_rate")), coalesce, 0) %>%
+   #    transmute(r, vacc_group_id, m = success_rate.x - success_rate.y) %>%
+   #    pivot_wider(names_from = vacc_group_id, values_from = m) %>%
+   #    select(-r) %>%
+   #    mutate_all(coalesce, 0) %>% 
+   #    select(moments_to_use)
+   
+   
+   objective <- moments %>% colMeans() %>% crossprod() %>% c()
+    
+    if (!is_null(logger)) {
+      logger$log(!!!model_probs, obj = objective) 
+    }
+   
+    if(calculate_objective) {
+      return(objective)
+    } else {
+      return(moments)
+    }
   }
 }
 
@@ -165,15 +179,39 @@ moments_to_use <- test_vcov %>%
   not() %>% 
   which()
 
-gmm_g <- build_gmm_g(candidate_data, 10e3, 50, use_corr_moments = FALSE, group_vaccines_by = group_vaccines_by, sim_seed = 123, moments_to_use = moments_to_use)
+test_log <- OptimLogger$new()
+
+gmm_g <- build_gmm_g(candidate_data, 10e3, 50, use_corr_moments = FALSE, group_vaccines_by = group_vaccines_by, sim_seed = 123,
+                     fixed_model_probs = c(pvector=0.8, psubunit=0.8, prna=0.6, pdna=0.4, pattenuated=0.8, pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5),
+                     logger = test_log) 
+
+
+# Test optimization -------------------------------------------------------
+
+test_optim <- optim(
+  fn = build_gmm_g(
+    candidate_data, test_summaries, 10e3, 50, use_vcov_moments = TRUE, group_vaccines_by = group_vaccines_by, sim_seed = 123,
+    # fixed_model_probs = c(prna = 0.6,  pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5),
+    fixed_model_probs = c(psubcat=0.9, pvector=0.8, psubunit=0.8, pdna=0.4, pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5),
+    calculate_objective = TRUE
+  ),
+  par = c(poverall = 0.5, pattenuated = 0.5, prna = 0.5) %>% qlogis()
+  # par = c(poverall = 0.5, psubcat = 0.5, pvector = 0.5, psubunit = 0.5, pdna = 0.5, pattenuated = 0.5) %>% qlogis(),
+  # control = lst(reltol = 0.00001)
+  # method = "BFGS"
+)
 
 # Test GMM run ------------------------------------------------------------
 
 test_results <- gmm(gmm_g, 
-                    test_draws, 
-                    # test_summaries, 
-                    t0 = c(poverall=0.9, psubcat=0.9, pvector=0.8, psubunit=0.8, prna=0.6, pdna=0.4, pattenuated=0.8, pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5) %>% qlogis(), 
-                    weightsMatrix = solve(test_vcov[moments_to_use, moments_to_use]))
+                    test_draws,  
+                    wmatrix = "ident",
+                    t0 = c(poverall = 0.5, psubcat = 0.5) %>% qlogis(), 
+                    # method = "Brent", lower = -10, upper = 10, 
+                    control = lst(reltol = 0.0001))
+                    # t0 = c(poverall=0.9, psubcat=0.9) %>% qlogis()) 
+#, pvector=0.8, psubunit=0.8, prna=0.6, pdna=0.4, pattenuated=0.8, pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5) %>% qlogis()) 
+                    # weightsMatrix = solve(test_vcov[moments_to_use, moments_to_use]))
                     #t0 = rep(0, 12)) # rnorm(12)) 
                     # type = "iterative") 
 
@@ -189,3 +227,9 @@ moment_landscape %>%
   ggplot() +
   geom_raster(aes(poverall_beta, psubcat_beta, fill = value)) +
   facet_wrap(vars(moment_id))
+
+test_log$data %>% 
+  ggplot() +
+  geom_line(aes(poverall, obj))
+  # geom_tile(aes(poverall, psubcat, fill = obj))
+  # geom_point(aes(poverall, psubcat, color = obj))
