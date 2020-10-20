3f97b99535da12e905154fa1acc2d507df5b057b
Karim Naguib <karimn2.0@gmail.com>
2020-10-20 14:11:11 -0400

    Ploting convergence and allowing grouping to  be on candidate

--- vac_gmm_fit.R
+++ vac_gmm_fit.R
@@ -48,19 +48,31 @@ get_candidate_draws <- function(candidate_data, replications,
       mutate(phase = fct_relabel(phase, str_replace_all, c("Phase(\\d)candidates" = "Phase \\1", "PreClinicalCandidates" = "Pre-clinical"))) %>% 
       group_by_at(group_vaccines_by) %>% 
       summarize(vacc_group_id = cur_group_id(), .groups = "drop")
-   
+  
     draws <- candidate_data %>% 
       candidatesFung(par) %>% 
       pluck("dordered") %>% 
       select(1:11) %>% 
       candidateDraws(par, seed = seed) %>% 
-      left_join(vacc_group_ids, by = setdiff(names(vacc_group_ids), "vacc_group_id")) %>% # Get the vaccine group IDs
-      group_by(r, vacc_group_id, .drop = FALSE) %>% 
-      summarize(any_success = sum(success) > 0, 
-                success_rate = mean(success),
-                .groups = "drop") %>%  # Any success in group
-      complete(r, vacc_group_id = seq(max(vacc_group_ids$vacc_group_id)), 
-               fill = lst(any_success = FALSE, success_rate = 0)) # Make sure all groups are present in the draws
+      left_join(vacc_group_ids, by = setdiff(names(vacc_group_ids), "vacc_group_id")) # Get the vaccine group IDs
+    
+    if (is_null(group_vaccines_by)) {
+      draws %<>% 
+        mutate(
+          any_success = success, 
+          success_rate = success,
+        )
+    } else {
+      draws %<>% 
+        group_by(r, vacc_group_id, .drop = FALSE) %>% 
+        summarize(any_success = sum(success) > 0, 
+                  success_rate = mean(success),
+                  .groups = "drop") %>%  # Any success in group
+        complete(r, vacc_group_id = seq(max(vacc_group_ids$vacc_group_id)), 
+                 fill = lst(any_success = FALSE, success_rate = 0)) # Make sure all groups are present in the draws
+    } 
+    
+    return(draws)
 }
 
 summarize_draws <- function(draws) {
@@ -104,14 +116,15 @@ OptimLogger <- R6Class("OptimLogger",
 
 build_gmm_g <- function(candidate_data, x, replications, maxcand, use_vcov_moments = FALSE, group_vaccines_by = vars(Platform, Subcategory), sim_seed = NULL, moments_to_use = everything(), fixed_model_probs = NULL, logger = NULL, calculate_objective = FALSE) {
   function(pbeta, ...) {
-    model_probs <- list_modify(as.list(fixed_model_probs), !!!plogis(pbeta))
+    model_probs <- plogis(pbeta)
+    all_model_probs <- list_modify(as.list(fixed_model_probs), !!!model_probs)
     
     model_probs %>% 
       imap(~ str_c(.y, " = ", .x)) %>% 
       str_c(collapse = ", ") %>% 
       cat("\n")
     
-    draws <- get_candidate_draws(candidate_data, replications = replications, !!!model_probs, maxcand = maxcand, group_vaccines_by = group_vaccines_by, seed = sim_seed)  
+    draws <- get_candidate_draws(candidate_data, replications = replications, !!!all_model_probs, maxcand = maxcand, group_vaccines_by = group_vaccines_by, seed = sim_seed)  
  
     model_summaries <- summarize_draws(draws) 
     
@@ -153,7 +166,7 @@ build_gmm_g <- function(candidate_data, x, replications, maxcand, use_vcov_momen
 
 # Settings ----------------------------------------------------------------
 
-group_vaccines_by <- vars(Platform, Subcategory, Target)
+group_vaccines_by <- vars(Platform, Subcategory, Target, phase)
 candidate_data <- load_candidate_data(file.path("data", "vaccinesSummaryOct2.csv"))
 
 # Test Data --------------------------------------------------------------------
@@ -161,7 +174,7 @@ candidate_data <- load_candidate_data(file.path("data", "vaccinesSummaryOct2.csv
 true_param <- c(poverall=0.9, psubcat=0.9, pvector=0.8, psubunit=0.8, prna=0.6, pdna=0.4, pattenuated=0.8, pinactivated=0.8, ppreclinical=0.14, pphase1=0.23, pphase2=0.32, pphase3=0.5)
 
 test_draws <- get_candidate_draws(
-  candidate_data, replications = 1e5,
+  candidate_data, replications = 3e5,
   !!!true_param,
   maxcand = 50,
   group_vaccines_by = group_vaccines_by
@@ -183,20 +196,37 @@ gmm_g <- build_gmm_g(candidate_data, 10e3, 50, use_vcov_moments = FALSE, group_v
                      fixed_model_probs = true_param, 
                      logger = test_log) 
 
-
 # Test optimization -------------------------------------------------------
+
+test_optim_log <- OptimLogger$new()
 
 test_optim <- optim(
   fn = build_gmm_g(
-    candidate_data, test_summaries, 10e3, 50, use_vcov_moments = TRUE, group_vaccines_by = group_vaccines_by, sim_seed = 123,
+    candidate_data, test_summaries, 20e3, 50, use_vcov_moments = TRUE, group_vaccines_by = group_vaccines_by, sim_seed = 123,
     fixed_model_probs = true_param,
-    calculate_objective = TRUE
+    calculate_objective = TRUE,
+    logger = test_optim_log
   ),
-  par = c(poverall = 0.5, pattenuated = 0.5, prna = 0.5) %>% qlogis(),
-  # par = c(poverall = 0.5, psubcat = 0.5, pvector = 0.5, psubunit = 0.5, pdna = 0.5, pattenuated = 0.5) %>% qlogis(),
+  par = c(poverall = 0.5, psubcat = 0.5, 
+          pvector = 0.5, psubunit = 0.5, prna = 0.5, pdna = 0.5,
+          pphase1 = 0.5, pphase2 = 0.5, pphase3 = 0.5) %>% qlogis(),
   control = lst(reltol = 0.00001)
   # method = "BFGS"
 )
+
+# Plot test optimization dynamics -----------------------------------------
+
+test_optim_log$data %>% 
+  mutate(step = seq(n())) %>% 
+  pivot_longer(-step, names_to = "param_name", values_to = "param_val") %>% 
+  ggplot() +
+  geom_line(aes(step, param_val)) +
+  geom_hline(aes(yintercept = value), 
+             linetype = "dotted",
+             data = . %>% semi_join(enframe(true_param, name = "param_name"), ., by = "param_name")) +
+  facet_wrap(vars(param_name), scales = "free") +
+  labs(y = "") +
+  theme_minimal()
 
 # Test GMM run ------------------------------------------------------------
 
@@ -230,3 +260,27 @@ test_log$data %>%
   geom_line(aes(poverall, obj))
   # geom_tile(aes(poverall, psubcat, fill = obj))
   # geom_point(aes(poverall, psubcat, color = obj))
+
+
+# Test candInd ------------------------------------------------------------
+
+test_draws1 <- get_candidate_draws(
+  candidate_data, replications = 1,
+  !!!true_param,
+  maxcand = 50,
+  group_vaccines_by = NULL 
+)
+
+test_draws2 <- get_candidate_draws(
+  candidate_data, replications = 1,
+  poverall=0.9, psubcat=0.5, pvector=0.8, psubunit=0.8, prna=0.3, pdna=0.8, pattenuated=0.8, pinactivated=0.8, ppreclinical=0.14, pphase1=0.6, pphase2=0.3, pphase3=0.2,
+  maxcand = 50,
+  group_vaccines_by = NULL 
+)
+
+full_join(
+  test_draws1 %>% 
+    select(candInd, Platform, Subcategory, Target, phase),
+  test_draws2 %>% 
+    select(candInd, Platform, Subcategory, Target, phase),
+  by = "candInd")
