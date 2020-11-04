503e59ea987aa1deb37d46574c6422a6ad595ec2
Karim Naguib <karimn2.0@gmail.com>
2020-11-03 22:18:01 -0500

    Latest cluster runs

--- cgd_fit_slurm.sh
+++ cgd_fit_slurm.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-#SBATCH -n 12               # Number of cores (-n)
+#SBATCH -n 24               # Number of cores (-n)
 #SBATCH -N 1                # Ensure that all cores are on one Node (-N)
-#SBATCH -t 0-06:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-03:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p shared           # serial_requeue   # Partition to submit to
-#SBATCH --mem=50000 # MB    # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=100000 # MB    # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o temp/log/cgd_fit_%j.log  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e temp/log/cgd_fit_%j.log  # File to which STDERR will be written, %j inserts jobid
 
-module load gcc R # gcc/9.2.0-fasrc01 R_core/3.6.3-fasrc01
+module load gcc R_core 
 
-Rscript cgd_fit.R --num-runs=10 --output=cgd_optim2
+#Rscript cgd_fit.R --num-runs=10 --parallel=$SLURM_NTASKS 
+Rscript cgd_fit.R --num-runs=10 --vcov-moments-weight=0.02 --output=cgd_optim_vcov_weighted.rds --parallel=$SLURM_NTASKS  
+# Rscript cgd_fit.R --num-runs=10 --output=cgd_optim_novcov --no-vcov-moments
 #Rscript cgd_fit.R --append-stage-2=data/cgd_optim.rds
 
